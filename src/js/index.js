49ac9e1b01d2b9db210bbaaaf9b123928fd3817b
John Cooley <johndavidcooley@gmail.com>
2016-03-10 16:52:26 -0500

    Added some minor changes. Pushing at the end of the day at TIY.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
 // Javascript Entry Point
+import etsyData from './items.js';
+
+import $ from 'jquery';
+
+var items = etsyData.results;
+
+
+function completeProduct() {
+	var productImage = items.Images[3];
+
+	return `${productImage}
+	$`;
+
+};
+
+
+
+items.forEach(fucntion() {
+	return completeProduct.append
+});
