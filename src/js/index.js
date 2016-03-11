97479a4fd2710857c2821ce3f5429428b1dfdb7c
John Cooley <johndavidcooley@gmail.com>
2016-03-11 14:29:14 -0500

    The JavaScript works!

--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,54 @@
 // Javascript Entry Point
-import etsyData from './items.js';
+import etsyData from './items';
 
 import $ from 'jquery';
 
 var items = etsyData.results;
 
+function makeWhiskeyProduct(whiskey) {
 
-function whiskeyProducts(whiskey) {
-	var whiskeyImage = items.Images[3];
+	var whiskeyImage = whiskey.Images[0].url_570xN;
+	var whiskeyTitle = whiskey.title;
+	var whiskeyDescription = whiskey.description;
+	var whiskeyStore = whiskey.Shop.url;
+	var whiskeyPrice = whiskey.price;
+	var whiskeyUSD = whiskey.currency_code;
+	var whiskeyPage = whiskey.url;
 
-	return `
+		return `
 	<div class="pulled">
 		<div class="whiskey-image">
-			<a href="items.url">
-				<img src="${whiskeyImage}" alt="${items.description}">
+			<a href="whiskeyPage">
+				<img src="${whiskeyImage}" alt="${whiskeyDescription}" width="225px" height="179px">
 			</a>
 		</div>
 		<div class="hover-images">
 			<a href="#">
-				<img src="/images/heart.png" alt="">
+				<img src="/images/heart.png" alt="Save to favorites">
 			</a>
 			<a href="#">
-				<img src="/images/hamburger.png" alt="">
+				<img src="/images/hamburger.png" alt="Save to favorites">
 			</a>
 		</div>
 		<div class="whiskey-title">
-			<a href="items.url">${items.title}</a>
+			<a href="items.url">${whiskeyTitle}</a>
 		</div>
 		<div class="whiskey-distiller">
-			<a href="items.url">${items.shop_name}</a>
+			<a href="items.url">${whiskeyPage}</a>
 		</div>
-		<div class="whiskey-price">${items.price}</div>
-		<div class="whiskey-usd">${items.currency_code}</div>
+		<div class="whiskey-price">${whiskeyPrice}</div>
+		<div class="whiskey-usd">${whiskeyUSD}</div>
 	</div>
 	`
 };
 
-
-
-items.forEach(function() {
-	$('pulled').append(whiskeyProducts());
+items.forEach(function(whiskey) {
+	$('.pulled-from-js').append(makeWhiskeyProduct(whiskey));
 });
 
 
 
+
+
+
+//Change line 38 to link to store.
