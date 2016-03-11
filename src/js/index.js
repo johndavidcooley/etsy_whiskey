bb0647b87ad63b8274846302ecacbb537e7f8506
John Cooley <johndavidcooley@gmail.com>
2016-03-11 00:38:41 -0500

    Made some changes. Made more mistakes.

--- src/js/index.js
+++ src/js/index.js
@@ -6,16 +6,41 @@ import $ from 'jquery';
 var items = etsyData.results;
 
 
-function completeProduct() {
-	var productImage = items.Images[3];
+function whiskeyProducts(whiskey) {
+	var whiskeyImage = items.Images[3];
 
-	return `${productImage}
-	$`;
-
+	return `
+	<div class="pulled">
+		<div class="whiskey-image">
+			<a href="items.url">
+				<img src="${whiskeyImage}" alt="${items.description}">
+			</a>
+		</div>
+		<div class="hover-images">
+			<a href="#">
+				<img src="/images/heart.png" alt="">
+			</a>
+			<a href="#">
+				<img src="/images/hamburger.png" alt="">
+			</a>
+		</div>
+		<div class="whiskey-title">
+			<a href="items.url">${items.title}</a>
+		</div>
+		<div class="whiskey-distiller">
+			<a href="items.url">${items.shop_name}</a>
+		</div>
+		<div class="whiskey-price">${items.price}</div>
+		<div class="whiskey-usd">${items.currency_code}</div>
+	</div>
+	`
 };
 
 
 
-items.forEach(fucntion() {
-	return completeProduct.append
+items.forEach(function() {
+	$('pulled').append(whiskeyProducts());
 });
+
+
+
