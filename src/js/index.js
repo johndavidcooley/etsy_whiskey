b2d2232980cea4075ebb845ffbcfa3f3a1542767
John Cooley <johndavidcooley@gmail.com>
2016-03-14 00:12:36 -0400

    Final commit before submitting assignment.
    There are still some things missing here, but hopefully they are only considered minor things.

--- src/js/index.js
+++ src/js/index.js
@@ -28,7 +28,7 @@ function makeWhiskeyProduct(whiskey) {
 				<img src="/images/heart.png" alt="Save to favorites" height="25">
 			</a>
 			<a href="#">
-				<img src="/images/hamburger.png" alt="Save to favorites"height="25">
+				<img src="/images/hamburger.png" alt="Save to favorites" height="25">
 			</a>
 		</div>
 		<div class="below-whiskey-image">
@@ -51,37 +51,10 @@ items.forEach(function(whiskey) {
 	$('.pulled-from-js').append(makeWhiskeyProduct(whiskey));
 });
 
-// <div class="pulled">
-// 		<div class="whiskey-image">
-// 			<a href="whiskeyPage">
-// 				<img src="${whiskeyImage}" alt="${whiskeyDescription}" width="225" height="179">
-// 			</a>
-// 		</div>
-// 		<div class="hover-images">
-// 			<a href="#">
-// 				<img src="/images/heart.png" alt="Save to favorites" height="25">
-// 			</a>
-// 			<a href="#">
-// 				<img src="/images/hamburger.png" alt="Save to favorites"height="25">
-// 			</a>
-// 		</div>
-// 		<div class="below-whiskey-image">
-// 			<div class="whiskey-title">
-// 				<a href="items.url">${whiskeyTitle}</a>
-// 			</div>
-// 			<div class="whiskey-distiller">
-// 				<a href="${whiskeyPage}">${whiskeyStoreName}</a>
-// 			</div>
-// 			<div class="price-usd">
-// 				<div class="whiskey-price">${whiskeyPrice}</div>
-// 				<div class="whiskey-usd">${whiskeyUSD}</div>
-// 			</div>
-// 		</div>
-// 	</div>
+
+$('.whiskey-image').on('hover', function() {
+	$('.hover-images').addClass('hover-active');
+});
 
 
 
-
-
-
-//Change line 38 to link to store.
